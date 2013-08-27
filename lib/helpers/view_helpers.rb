bb5755e54a26664c63c25e700a40740756b084d2
Daniel LaBare <dlabare@gmail.com>
2013-08-27 12:22:33 -0700

    minor updates, add options for skipping column validation, and whether or not to store order in session

--- lib/helpers/view_helpers.rb
+++ lib/helpers/view_helpers.rb
@@ -2,22 +2,23 @@ module SortableColumns
   
   module ViewHelpers
 
-    def sort_params(sortable, column)
+    def sort_params(sortable, column, opts = {})
       raise ParameterError.new("Please provide a Class as your first param. Ex: sort_param(User, :created_at)") unless sortable.is_a?(Class)
-      raise ParameterError.new("#{sortable} has no column \"#{column}\".") unless sortable.column_names.include?(column.to_s)
+      raise ParameterError.new("#{sortable} has no column \"#{column}\".") unless sortable.column_names.include?(column.to_s) || opts[:skip_column_validation]
       
       init_session unless session[:sortable_columns]
+      return_params = params.dup.except('action', 'controller')
       
       if session[:sortable_columns][sortable.to_s.downcase.to_sym]
         if session[:sortable_columns][sortable.to_s.downcase.to_sym][column.to_sym] == "asc"
-          return { :sort_by => column.to_s, :order => 'desc' }
+          return return_params.merge(:sort_by => column.to_s, :order => 'desc')
         else
-          return { :sort_by => column.to_s, :order => 'asc' }  
+          return return_params.merge(:sort_by => column.to_s, :order => 'asc')
         end
       end
       
       # default
-      return { :sort_by => column.to_s, :order => 'desc' }
+      return return_params.merge(:sort_by => column.to_s, :order => 'desc')
     end
   
   private
--- lib/sortable_columns.rb
+++ lib/sortable_columns.rb
@@ -5,24 +5,26 @@ module SortableColumns
   class ParameterError < StandardError; end
   
   def sortable_order(sortable, options = {})
-    if params[:sort_by] && params[:order]
-      validate_params(sortable)
+    if params[:sort_by] && params[:order] && !params[:sort_by].blank? && !params[:order].blank?
+      validate_params(sortable, options)
       store_sort(sortable)
-      return "#{params.delete(:sort_by)} #{params.delete(:order)}"
-    else
+      return "#{params.dup.delete(:sort_by)} #{params.dup.delete(:order)}"
+    elsif options[:use_session]
       if session[:sortable_columns] && session[:sortable_columns][sortable.to_s.downcase.to_sym]
         column = session[:sortable_columns][sortable.to_s.downcase.to_sym].keys.first
         return "#{column.to_s} #{session[:sortable_columns][sortable.to_s.downcase.to_sym][column.to_sym]}"
       else
         return options[:default]
       end
+    else
+      return options[:default]
     end
   end
   
 private
 
-  def validate_params(sortable)
-    raise ParameterError.new("#{sortable} has no column \"#{params[:sort_by]}\".") unless sortable.column_names.include?(params[:sort_by])
+  def validate_params(sortable, options)
+    raise ParameterError.new("#{sortable} has no column \"#{params[:sort_by]}\".") unless sortable.column_names.include?(params[:sort_by]) || options[:skip_column_validation]
     raise ParameterError.new("Order must be \"asc\" or \"desc\"") unless params[:order] == "asc" || params[:order] == "desc"
   end
 
